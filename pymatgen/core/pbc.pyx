5b3a659a9eec893c9d19be53ba89f445c800cd82
__sp__ <ongsp@eng.ucsd.edu>
2014-10-24 10:24:33 -0700

    Some more cython testing. Still doesn't seem to be worth it.

--- pymatgen/core/pbc.pyx
+++ pymatgen/core/pbc.pyx
@@ -14,7 +14,9 @@ ctypedef np.float_t DTYPE_t
 
 
 cpdef cget_distance_and_image(np.ndarray[DTYPE_t, ndim=2] lattice,
-                             frac_coords1, frac_coords2, jimage=None):
+                              np.ndarray[DTYPE_t, ndim=1] frac_coords1,
+                              np.ndarray[DTYPE_t, ndim=1] frac_coords2,
+                              jimage=None):
     """
     Gets distance between two frac_coords assuming periodic boundary
     conditions. If the index jimage is not specified it selects the j
@@ -38,31 +40,30 @@ cpdef cget_distance_and_image(np.ndarray[DTYPE_t, ndim=2] lattice,
         the distance between frac_coords1 and (jimage + frac_coords2) is
         equal to distance.
     """
-    cdef np.ndarray[DTYPE_t, ndim=1] adj1, adj2, coord1, coord2, vec
+    cdef float i, j, k
+    cdef np.ndarray[DTYPE_t, ndim=1] vec, coord1, coord2, image, shortest_image
     cdef float d
     cdef float dist = float('inf')
+    cdef np.ndarray[DTYPE_t, ndim=1] adj1 = np.floor(frac_coords1)
+    cdef np.ndarray[DTYPE_t, ndim=1] adj2 = np.floor(frac_coords2)
 
     if jimage is None:
-        #The following code is heavily vectorized to maximize speed.
-        #Get the image adjustment necessary to bring coords to unit_cell.
-        adj1 = np.floor(frac_coords1)
-        adj2 = np.floor(frac_coords2)
         #Shift coords to unitcell
         coord1 = frac_coords1 - adj1
         coord2 = frac_coords2 - adj2
         # Generate set of images required for testing.
         # This is a cheat to create an 8x3 array of all length 3
         # combinations of 0,1
-        r = (-1, 0, 1)
-
+        r = (-1.0, 0.0, 1.0)
         for i, j, k in itertools.product(r, r, r):
-            vec = coord2 - coord1 + [i, j, j]
+            image = np.array([i, j, k])
+            vec = coord2 - coord1 + image
             vec = np.dot(vec, lattice)
-            d = np.linalg.norm(vec)
+            d = (vec[0] ** 2 + vec[1] ** 2 + vec[2] ** 2) ** 0.5
             if d < dist:
                 dist = d
-                image = [i, j, k]
-        return dist, adj1 - adj2 + image
+                shortest_image = image
+        return dist, adj1 - adj2 + shortest_image
 
     mapped_vec = np.dot(jimage + frac_coords2 - frac_coords1, lattice)
     return np.linalg.norm(mapped_vec), jimage
@@ -120,3 +121,166 @@ def get_distance_and_image(lattice, frac_coords1, frac_coords2, jimage=None):
 
     mapped_vec = np.dot(jimage + frac_coords2 - frac_coords1, lattice)
     return np.linalg.norm(mapped_vec), jimage
+
+
+cpdef np.ndarray[DTYPE_t, ndim=2] cget_lll_reduced_lattice(
+        np.ndarray[DTYPE_t, ndim=2] lattice, float delta=0.75):
+    """
+    Performs a Lenstra-Lenstra-Lovasz lattice basis reduction to obtain a
+    c-reduced basis. This method returns a basis which is as "good" as
+    possible, with "good" defined by orthongonality of the lattice vectors.
+
+    Args:
+        delta (float): Reduction parameter. Default of 0.75 is usually
+            fine.
+
+    Returns:
+        Reduced lattice.
+    """
+    cdef int i, k
+    cdef float q
+
+    # Transpose the lattice matrix first so that basis vectors are columns.
+    # Makes life easier.
+    cdef np.ndarray[DTYPE_t, ndim=2] a = lattice.T
+
+    cdef np.ndarray[DTYPE_t, ndim=2] b = np.zeros((3, 3))  # Vectors after the
+    # Gram-Schmidt process
+    cdef np.ndarray[DTYPE_t, ndim=2] u = np.zeros((3, 3))  # Gram-Schmidt
+    # coeffieicnts
+    cdef np.ndarray[DTYPE_t, ndim=1] m = np.zeros(3)  # These are the norm
+    # squared
+    #  of each vec.
+    cdef np.ndarray[DTYPE_t, ndim=2] p, qq, result
+    cdef np.ndarray[DTYPE_t, ndim=1] v, uu
+
+    dot = np.dot
+
+    b[:, 0] = a[:, 0]
+    m[0] = dot(b[:, 0], b[:, 0])
+    for i in range(1, 3):
+        u[i, 0:i] = dot(a[:, i].T, b[:, 0:i]) / m[0:i]
+        b[:, i] = a[:, i] - dot(b[:, 0:i], u[i, 0:i].T)
+        m[i] = dot(b[:, i], b[:, i])
+
+    k = 2
+
+    while k <= 3:
+        # Size reduction.
+        for i in range(k - 1, 0, -1):
+            q = round(u[k - 1, i - 1])
+            if q != 0:
+                # Reduce the k-th basis vector.
+                a[:, k - 1] = a[:, k - 1] - q * a[:, i - 1]
+                uu = u[i - 1, 0:(i - 1)]
+                uu = np.append(uu, 1)
+                # Update the GS coefficients.
+                u[k - 1, 0:i] = u[k - 1, 0:i] - q * uu
+
+        # Check the Lovasz condition.
+        if dot(b[:, k - 1], b[:, k - 1]) >=\
+                (delta - abs(u[k - 1, k - 2]) ** 2) *\
+                dot(b[:, (k - 2)], b[:, (k - 2)]):
+            # Increment k if the Lovasz condition holds.
+            k += 1
+        else:
+            #If the Lovasz condition fails,
+            #swap the k-th and (k-1)-th basis vector
+            v = a[:, k - 1].copy()
+            a[:, k - 1] = a[:, k - 2].copy()
+            a[:, k - 2] = v
+            #Update the Gram-Schmidt coefficients
+            for s in range(k - 1, k + 1):
+                u[s - 1, 0:(s - 1)] = dot(a[:, s - 1].T,
+                                          b[:, 0:(s - 1)]) / m[0:(s - 1)]
+                b[:, s - 1] = a[:, s - 1] - dot(b[:, 0:(s - 1)],
+                                                u[s - 1, 0:(s - 1)].T)
+                m[s - 1] = dot(b[:, s - 1], b[:, s - 1])
+
+            if k > 2:
+                k -= 1
+            else:
+                # We have to do p/q, so do lstsq(q.T, p.T).T instead.
+                p = dot(a[:, k:3].T, b[:, (k - 2):k])
+                qq = np.diag(m[(k - 2):k])
+                result = np.linalg.lstsq(qq.T, p.T)[0].T
+                u[k:3, (k - 2):k] = result
+
+    return a.T
+
+
+def get_lll_reduced_lattice(lattice, float delta=0.75):
+    """
+    Performs a Lenstra-Lenstra-Lovasz lattice basis reduction to obtain a
+    c-reduced basis. This method returns a basis which is as "good" as
+    possible, with "good" defined by orthongonality of the lattice vectors.
+
+    Args:
+        delta (float): Reduction parameter. Default of 0.75 is usually
+            fine.
+
+    Returns:
+        Reduced lattice.
+    """
+    # Transpose the lattice matrix first so that basis vectors are columns.
+    # Makes life easier.
+    a = lattice.T
+
+    b = np.zeros((3, 3))  # Vectors after the Gram-Schmidt process
+    u = np.zeros((3, 3))  # Gram-Schmidt coeffieicnts
+    m = np.zeros(3)  # These are the norm squared of each vec.
+
+    b[:, 0] = a[:, 0]
+
+
+    dot = np.dot
+    m[0] = dot(b[:, 0], b[:, 0])
+    for i in range(1, 3):
+        u[i, 0:i] = dot(a[:, i].T, b[:, 0:i]) / m[0:i]
+        b[:, i] = a[:, i] - dot(b[:, 0:i], u[i, 0:i].T)
+        m[i] = dot(b[:, i], b[:, i])
+
+    k = 2
+
+    while k <= 3:
+        # Size reduction.
+        for i in range(k - 1, 0, -1):
+            q = round(u[k - 1, i - 1])
+            if q != 0:
+                # Reduce the k-th basis vector.
+                a[:, k - 1] = a[:, k - 1] - q * a[:, i - 1]
+                uu = list(u[i - 1, 0:(i - 1)])
+                uu.append(1)
+                # Update the GS coefficients.
+                u[k - 1, 0:i] = u[k - 1, 0:i] - q * np.array(uu)
+
+        # Check the Lovasz condition.
+        if dot(b[:, k - 1], b[:, k - 1]) >=\
+                (delta - abs(u[k - 1, k - 2]) ** 2) *\
+                dot(b[:, (k - 2)], b[:, (k - 2)]):
+            # Increment k if the Lovasz condition holds.
+            k += 1
+        else:
+            #If the Lovasz condition fails,
+            #swap the k-th and (k-1)-th basis vector
+            v = a[:, k - 1].copy()
+            a[:, k - 1] = a[:, k - 2].copy()
+            a[:, k - 2] = v
+            #Update the Gram-Schmidt coefficients
+            for s in range(k - 1, k + 1):
+                u[s - 1, 0:(s - 1)] = dot(a[:, s - 1].T,
+                                          b[:, 0:(s - 1)]) / m[0:(s - 1)]
+                b[:, s - 1] = a[:, s - 1] - dot(b[:, 0:(s - 1)],
+                                                u[s - 1, 0:(s - 1)].T)
+                m[s - 1] = dot(b[:, s - 1], b[:, s - 1])
+
+            if k > 2:
+                k -= 1
+            else:
+                # We have to do p/q, so do lstsq(q.T, p.T).T instead.
+                p = dot(a[:, k:3].T, b[:, (k - 2):k])
+                q = np.diag(m[(k - 2):k])
+                result = np.linalg.lstsq(q.T, p.T)[0].T
+                u[k:3, (k - 2):k] = result
+
+    return a.T
--- test_pbc.py
+++ test_pbc.py
@@ -1,18 +1,29 @@
 # coding: utf-8
 
-from pymatgen.core.pbc import get_distance_and_image, cget_distance_and_image
+from pymatgen.core.pbc import get_distance_and_image, \
+    cget_distance_and_image, cget_lll_reduced_lattice, get_lll_reduced_lattice
 import numpy as np
 
-lattice = np.eye(3) * 3
+lattice = np.array([[1.0, 1, 1], [-1.0, 0, 2], [3.0, 5, 6]])
 
 def cfunc():
-    cget_distance_and_image(lattice, [0.1, 0.1, 0.1], [0.9, 0.9, 0.9])
+    # cget_distance_and_image(lattice,
+    #                         np.array([0.1, 0.1, 0.1]),
+    #                         np.array([0.9, 0.9, 0.9]))
+    cget_lll_reduced_lattice(lattice)
 
 def pyfunc():
-    get_distance_and_image(lattice, [0.1, 0.1, 0.1], [0.9, 0.9, 0.9])
+    #get_distance_and_image(lattice, [0.1, 0.1, 0.1], [0.9, 0.9, 0.9])
+    get_lll_reduced_lattice(lattice)
 
 if __name__ == "__main__":
     import timeit
-    print(timeit.timeit("cfunc()", setup="from __main__ import cfunc", number=1))
+    print(timeit.timeit("cfunc()", setup="from __main__ import cfunc",
+                        number=10))
     print(timeit.timeit("pyfunc()", setup="from __main__ import pyfunc",
-        number=1))
+        number=10))
+    import cProfile, pstats, os
+    cProfile.run('cfunc()', 'stats')
+    p = pstats.Stats('stats')
+    p.sort_stats('cumulative').print_stats(20)
+    os.remove('stats')
