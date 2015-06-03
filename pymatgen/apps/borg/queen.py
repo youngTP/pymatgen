708d27be33810e2e7893e0e84951b35db00b2c22
czhengsci <czhengsci@gmail.com>
2015-06-02 21:27:10 -0700

    debug hive

--- pymatgen/apps/borg/queen.py
+++ pymatgen/apps/borg/queen.py
@@ -75,6 +75,7 @@ class BorgQueen(object):
         status['count'] = 0
         status['total'] = len(valid_paths)
         logger.info('{} valid paths found.'.format(len(valid_paths)))
+        logging.debug('Data info is {}'.format(data))
         p = Pool(self._num_drones)
         p.map(order_assimilation, ((path, self._drone, data, status)
                                    for path in valid_paths))
