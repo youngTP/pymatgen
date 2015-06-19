02564ff8c58795462dbe4e257f523b8c30e5b262
vivid0036 <vivid0036@gmail.com>
2015-06-19 11:38:50 -0700

    add incar_settings_basic, only keep MPSlabVaspInputSet and get_input_mp()

--- pymatgen/core/metal_surf_task.py
+++ pymatgen/core/metal_surf_task.py
@@ -6,22 +6,8 @@ __version__ = "0.1"
 __email__ = "vivid0036@gmail.com"
 __date__ = "6/2/15"
 
-import itertools
-import json
-import os
-import os.path as pth
 
-from pymongo import MongoClient
-from fireworks.core.firework import FireTaskBase, FWAction
-from fireworks.utilities import fw_utilities
-from fireworks import explicit_serialize
-from pymatgen.core.structure import Structure
-from pymatgen.io.vaspio.vasp_output import Vasprun
 from pymatgen.io.vaspio_set import MPVaspInputSet, DictVaspInputSet
-from custodian.custodian import Custodian
-from custodian.vasp.jobs import VaspJob
-from matgendb.creator import VaspToDbTaskDrone
-
 from pymatgen.core.surface import Slab, SlabGenerator
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 from pymatgen.matproj.rest import MPRester
@@ -37,27 +23,31 @@ class MPSlabVaspInputSet(DictVaspInputSet):
     Args:
         user_incar_settings(dict): A dict specifying additional incar
             settings, default to None
+            (ediff_per_atom=False)
         kpoints0: specify kpts[0] = kpoints0, default to []
         k_product: kpts[0][0]*a. Decide k density without kpoint0,
         default to 45
-        potcar_functional: default to LDA
+        potcar_functional: default to PBE
         bulk: default to False
+
 
         **kwargs:
             Other kwargs supported by :class:`DictVaspInputSet`.
     """
     def __init__(self, user_incar_settings=None, kpoints0=[],
-                  k_product=45, potcar_functional='LDA', bulk=False, **kwargs):
-        vis = MPVaspInputSet().as_dict()
+                  k_product=45, potcar_functional='PBE', bulk=False, **kwargs):
+        vis = MPVaspInputSet(ediff_per_atom=False).as_dict()
         DictVaspInputSet.__init__(self, "MaterialsProject Slab", vis["config_dict"],
                                   **kwargs)
+        incar_settings_basic = {"NPAR": 4, "AMIN": 0.01, "AMIX": 0.2, "BMIX": 0.001,
+                                "EDIFF": 0.001, "EDIFFG": -0.05, "ENCUT": 400,
+                                "ISMEAR": 1, "SIGMA": 0.05, "ISIF": 3}
+
         if bulk:
-             self.incar_settings.update(
-                 {"NPAR": 4})
+             self.incar_settings.update(incar_settings_basic)
         else:
-            self.incar_settings.update(
-                {"AMIN": 0.01, "AMIX": 0.2, "BMIX": 0.001, "ISIF": 2,
-                 "NPAR": 4, "NELMIN": 8})
+            incar_settings_basic["ISIF"] = 2
+            self.incar_settings.update(incar_settings_basic)
         self.user_incar_settings = user_incar_settings or {}
         if user_incar_settings:
             self.incar_settings.update(user_incar_settings)
@@ -152,10 +142,6 @@ class MPSlabVaspInputSet(DictVaspInputSet):
         return data
 
 
-"""
-Firework tasks
-"""
-
 def get_input_mp(element, miller_index, api_key, min_slab_size=10, min_vacuum_size=10,
                  symprec=0.001, angle_tolerance=5):
     """
@@ -176,119 +162,5 @@ def get_input_mp(element, miller_index, api_key, min_slab_size=10, min_vacuum_si
     oriented_u_cell = slab_metal.oriented_unit_cell
     slab_cell = slab_metal.get_slab()
 
-
-
     return oriented_u_cell, slab_cell
-
-@explicit_serialize
-class WriteSurfVaspInput(FireTaskBase):
-    """writes VASP inputs given elements, hkl,  """
-
-    required_params = ["element", "miller_index", "api_key"]
-    optional_params = ["min_slab_size", "min_vacuum_size",
-                       "symprec", "angle_tolerance", "user_incar_settings",
-                       "k_product","potcar_functional"]
-
-    def run_task(self, fw_spec):
-        dec = MontyDecoder()
-        element = dec.process_decoded(self.get("element"))
-        miller_index = dec.process_decoded(self.get("miller_index"))
-        api_key = dec.process_decoded(self.get("api_key"))
-        min_slab_size= dec.process_decoded(self.get("min_slab_size", 10))
-        min_vacuum_size = dec.process_decoded(self.get("min_vacuum_size", 10))
-        symprec = dec.process_decoded(self.get("symprec", 0.001))
-        angle_tolerance = dec.process_decoded(self.get("angle_tolerance", 5))
-        user_incar_settings = dec.process_decoded(self.get("user_incar_settings", 
-                                                           {'ISIF': 2, 'EDIFFG':  -0.05,'EDIFF': 0.0001,
-                                                            'ISMEAR': 1,'AMIX': 0.1,'BMIX': 0.0001, 
-                                                            'AMIX_MAG': 0.4, 'BMIX_MAG': 0.0001, 
-                                                            'NPAR':4, 'SIGMA': 0.05}))
-        k_product = dec.process_decoded(self.get("k_product", 50))
-        potcar_functional = dec.process_decoded(self.get("potcar_fuctional", 'LDA'))
-        
-        
-        input_structures = get_input_mp(element, miller_index, api_key, min_slab_size, 
-                                        min_vacuum_size,symprec, angle_tolerance)
-
-        orient_u_cell = input_structures[0]
-        slab_cell = input_structures[1]
-        mplb_u = MPSlabVaspInputSet(user_incar_settings=user_incar_settings, k_product=k_product, 
-                                    potcar_funtional=potcar_functional, bulk = True)
-        mplb_u.write_input(orient_u_cell, '%s_ucell_k%s' %element %k_product)
-        
-        mplb_s = MPSlabVaspInputSet(user_incar_settings=user_incar_settings, k_product=k_product, 
-                                    potcar_funtional=potcar_functional, bulk = False)
-        mplb_s.write_input(slab_cell, '%s_scell_k%s' %element %k_product)
-        
-@explicit_serialize
-class RunCustodianTask(FireTaskBase):
-    """Runs Custodian."""
-
-    required_params = ["jobs"]
-    optional_params = ["custodian_params"]
-
-    def run_task(self, fw_spec):
-
-        fw_env = fw_spec.get("_fw_env", {})
-        cust_params = self.get("custodian_params", {})
-        if fw_env.get('scratch_root'):
-            cust_params['scratch_dir'] = os.path.expandvars(
-                fw_env['scratch_root'])
-
-        dec = MontyDecoder()
-        #handlers = dec.process_decoded(self['handlers'])
-        jobs = dec.process_decoded(self['jobs'])
-        #validators = [VasprunXMLValidator()]
-        handlers = [VaspErrorHandler(), MeshSymmetryErrorHandler(),
-                    UnconvergedErrorHandler(), NonConvergingErrorHandler(),
-                    PotimErrorHandler()]
-
-        c = Custodian(handlers, jobs, max_errors=10, **cust_params)
-        output = c.run()
-
-        return FWAction(stored_data=output)
-
-
-@explicit_serialize
-class VaspDBInsertTask(FireTaskBase):
-
-    required_params = ["host", "port", "user", "password", "database",
-                       "collection", "struct_type", "miller_index"]
-
-    def run_task(self, fw_spec):
-
-        dec = MontyDecoder()
-        miller_index = dec.process_decoded(self.get("miller_index"))
-        struct_type = dec.process_decoded(self.get("struct_type"))
-
-        if not self["host"]:
-            self["host"] = "127.0.0.1"
-
-        if not self["port"]:
-            self["port"] = 27017
-
-        if not self["database"]:
-            self["database"] = "vasp"
-
-        if not self["collection"]:
-            self["collection"] = "tasks"
-
-        try:
-            with open("FINAL_LOC") as f:
-                loc = f.read()
-        except IOError:
-            loc = "."
-
-        with open(pth.join("/projects/ong-group/repos/pymacy/resources/db/",
-                            "db.json")) as f:
-            d = json.load(f)
-            drone = VaspToDbTaskDrone(host=self["host"], port=self["port"],
-                                      user=self["user"], password=self["password"],
-                                      database=self["database"], collection=self["collection"],
-                                      mapi_key=d["mapi_key"],
-                                      additional_fields={"author": os.environ.get("USER"),
-                                                         "type": struct_type,
-                                                         "miller index": miller_index},
-                                      use_full_uri=False)
-            drone.assimilate(loc)
 
