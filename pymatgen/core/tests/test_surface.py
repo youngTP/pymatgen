177d1d8adbf9f7f299faf5d9de2821ceb0e7ea41
Richard Tran <rit634989@gmail.com>
2015-10-28 17:01:27 -0700

    replaced get_symmetrically_distinct_miller_indices with GetMillerIndices class, can now get equivalent indices as well

--- pymatgen/core/tests/test_surface.py
+++ pymatgen/core/tests/test_surface.py
@@ -10,7 +10,7 @@ import numpy as np
 from pymatgen.core.structure import Structure
 from pymatgen.core.lattice import Lattice
 from pymatgen.core.surface import Slab, SlabGenerator, generate_all_slabs, \
-    get_symmetrically_distinct_miller_indices
+    GetMillerIndices
 from pymatgen.symmetry.groups import SpaceGroup
 from pymatgen.util.testing import PymatgenTest
 
@@ -245,26 +245,31 @@ class FuncTest(PymatgenTest):
                             ["H", "He"], [[0, 0, 0], [0.1, 0.2, 0.3]])
         self.graphite = self.get_structure("Graphite")
 
-    def test_get_symmetrically_distinct_miller_indices(self):
-        indices = get_symmetrically_distinct_miller_indices(self.cscl, 1)
+    def test_GetMillerIndices(self):
+        indices = GetMillerIndices(self.cscl, 1).\
+            get_symmetrically_distinct_miller_indices
         self.assertEqual(len(indices), 3)
-        indices = get_symmetrically_distinct_miller_indices(self.cscl, 2)
+        indices = GetMillerIndices(self.cscl, 2).\
+            get_symmetrically_distinct_miller_indices
         self.assertEqual(len(indices), 6)
 
-        self.assertEqual(len(get_symmetrically_distinct_miller_indices(
-                         self.lifepo4, 1)), 7)
+        self.assertEqual(len(GetMillerIndices(self.lifepo4, 1).
+                             get_symmetrically_distinct_miller_indices), 7)
 
         # The TeI P-1 structure should have 13 unique millers (only inversion
         # symmetry eliminates pairs)
-        indices = get_symmetrically_distinct_miller_indices(self.tei, 1)
+        indices = GetMillerIndices(self.tei, 1).\
+            get_symmetrically_distinct_miller_indices
         self.assertEqual(len(indices), 13)
 
         # P1 and P-1 should have the same # of miller indices since surfaces
         # always have inversion symmetry.
-        indices = get_symmetrically_distinct_miller_indices(self.p1, 1)
+        indices = GetMillerIndices(self.p1, 1).\
+            get_symmetrically_distinct_miller_indices
         self.assertEqual(len(indices), 13)
 
-        indices = get_symmetrically_distinct_miller_indices(self.graphite, 2)
+        indices = GetMillerIndices(self.graphite, 2).\
+            get_symmetrically_distinct_miller_indices
         self.assertEqual(len(indices), 12)
 
     def test_generate_all_slabs(self):
