d0f94c6fba2164943e132ad0e12f0427d1e8f46b
montoyjh <montoyjh@gmail.com>
2017-06-04 16:35:07 -0700

    added option to construct kpoints directly from kwargs

--- pymatgen/io/vasp/sets.py
+++ pymatgen/io/vasp/sets.py
@@ -367,13 +367,18 @@ class DictSet(VaspInputSet):
         elif settings.get('length'):
             return Kpoints.automatic(settings['length'])
 
+        # If direct_kwargs, construct kpoints from kwargs directly
+        elif settings.get('direct_kwargs'):
+            return Kpoints(**settings['direct_kwargs'])
+        
         # Raise error. Unsure of which kpoint generation to use
         else:
             raise ValueError(
-                "Invalid KPoint Generation algo : Supported Keys are "
+                "Invalid KPoint Generation: Supported Keys are "
                 "grid_density: for Kpoints.automatic_density generation, "
                 "reciprocal_density: for KPoints.automatic_density_by_vol "
-                "generation, and length  : for Kpoints.automatic generation")
+                "generation, length  : for Kpoints.automatic generation "
+                "and direct_kwargs : for use of the default Kpoints constructor")
 
     def __str__(self):
         return self.__class__.__name__
